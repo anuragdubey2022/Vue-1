b7254efc745298d08b841cd4128a75bfd9ad5813
anuragdubey2022 <126174923+anuragdubey2022@users.noreply.github.com>
2023-04-16 19:21:00 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Vue-1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-pnlstm)
---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="https://vuejs.org/images/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <span id="a">{{ key }}</span>
+
+    <button @click="test()"></button>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+
+export default {
+  name: 'App',
+
+  data() {
+    return {
+      key: 'hello',
+    };
+  },
+  methods: {
+    test() {
+      this.key = 'world';
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
